45f1a1afa18302306ebf08d623c6a235fea6eec2
Guilherme Mendes <guilherme@gamgconsult.com.br>
2023-06-25 10:38:39 -0300

    Migrations

---
+++ database/migrations/2023_06_25_025740_create_beneficiaries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeneficiariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beneficiaries', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('cfp');
+            $table->string('name',60);
+            $table->integer('phone');
+            $table->date('brith');
+            $table->string('email',60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beneficiaries');
+    }
+}
---
+++ database/migrations/2023_06_25_040308_create_suggestion_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuggestionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suggestion', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('schedule_id');
+            $table->string('semana',12);
+            $table->string('periodo',5);
+        });
+        Schema::table('suggestion', function (Blueprint $table) {
+            $table->foreach('schedule_id')->references('id')->on('schedule');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suggestion');
+    }
+}
