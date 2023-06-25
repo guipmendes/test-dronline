b009c2dad89d884a0fd52c10a47c97a51cdab430
Guilherme Mendes <guilherme@gamgconsult.com.br>
2023-06-25 10:37:51 -0300

    migrations

---
+++ database/migrations/2023_06_25_031612_create_schedule_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->date('dtsolicitacao');
+            $table->string('especialidade',30);
+            $table->string('status',40);
+            $table->text('observacao');
+        });
+
+        Schema::table('schedule', function (Blueprint $table) {
+            $table->foreach('user_id')->references('id')->on('beneficiaries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule');
+    }
+}
